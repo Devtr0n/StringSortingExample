f4d6836033f6100595376ee9b88d8d4d5a5c4085
Devtr0n <richardsmailbox@gmail.com>
2015-04-27 14:06:48 -0500

    initial upload of programming exercise

---
+++ src/StringSortExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+/*************************************************************************
+ *
+ * Description:
+ * This file contains a simple string sorting example, which makes use of LINQ.
+ * 
+ * Author:
+ * Richard Hollon - 4/27/2015
+ *
+ *************************************************************************/
+ 
+namespace StringSortExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "0011001101010101011111110000000111111000101010101010101010";    //input
+            char[] list = input.ToCharArray();                                              //data structure
+            var sb = new StringBuilder();                                                   //output
+
+            foreach (var item in list.OrderBy(x=>x)) //order ascending
+            {
+                sb.Append(item); //concat output string
+            }
+
+            Console.WriteLine(sb.ToString()); //display output
+        }
+    }
+}
